663b411393e2e6143b6fce101338e90642528fcc
tushArt <tusharpalase4614@gmail.com>
2026-07-22 21:26:33 +0530

    Second problem

---
+++ List-problems/ListTwo.py
@@ -0,0 +1,10 @@
+#2. Write a Python program to multiply all the items in a list.
+
+lst = [7,15,10,11,1,23]
+
+mulofall = 1
+
+for num in lst:
+    mulofall *= num
+
+print("Multiplication of all items for the list :", mulofall)
